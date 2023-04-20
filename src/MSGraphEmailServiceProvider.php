75caae9790e4ed364c8f73df0d29cb05d176d50b
achamchi <achamchi@actando.com>
2023-04-20 13:18:39 +0000

    add variables to dotenv

--- src/MSGraphEmailServiceProvider.php
+++ src/MSGraphEmailServiceProvider.php
@@ -3,6 +3,7 @@
 namespace rednucleus\Emailsender;
 
 use Exception;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\ServiceProvider;
 use LaravelMsGraphMail\Exceptions\CouldNotSendMail;
@@ -28,7 +29,56 @@ class MSGraphEmailServiceProvider extends ServiceProvider
 
     public function register()
     {
-        $this->app->register('rednucleus\Emailsender\MSGraphEmailServiceProvider');
+        $this->setEnv('MS_CLIENT_ID');
+        $this->setEnv('MS_CLIENT_SECRET');
+        $this->setEnv('MS_TENANT_ID');
+        $this->setEnv('MS_REFRESH_TOKEN');
+    }
+
+    private function setEnv($key, $value = '')
+    {
+        $path = base_path('.env');
+
+        try {
+            if (file_exists($path)) {
+
+                // Get all the lines from that file
+                $lines = explode("\n", file_get_contents($path));
+
+                $settings = collect($lines)
+                    ->filter() // remove empty lines
+                    ->transform(function ($item) {
+                        return explode("=", $item, 2);
+                    }) // separate key and values
+                    ->pluck(1, 0); // keys to keys, values to values
+
+                if (!isset($settings[$key]))
+                    $settings[$key] = $value; // set the new value whether it exists or not
+                $previousKey = null;
+                $rebuilt = $settings->map(function ($value, $key) use (&$previousKey) {
+                    $line = "";
+                    if ($previousKey == null) {
+                        $previousKey = $key;
+                    }
+
+                    $first_word_1 = substr($previousKey, 0, strpos($previousKey, '_'));
+                    $first_word_2 = substr($key, 0, strpos($key, '_'));
+
+                    // Compare the two words
+                    if ($first_word_1 != $first_word_2) {
+                        $line = "\n";
+                    }
+
+                    $line .= "$key=$value";
+                    $previousKey = $key;
+                    return $line;
+                })->implode("\n"); // rebuild the env file
+
+                file_put_contents($path, $rebuilt); // put the new contents
+            }
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        }
     }
 
 }
